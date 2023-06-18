6a523912bda4c8339695c204ba0ff0578a93f405
radall1 <radall1@stolaf.edu>
2023-06-17 19:33:18 -0500

    version 1.0

--- app.js
+++ app.js
@@ -1,128 +1,54 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 49236;
+const port = process.env.PORT || 8080;
 
 const mysql = require('mysql2');
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
+const fs = require('fs');
+
 connection.connect();
 app.use(express.static('public'));
-//app.use('/favicon.ico', (req, res) => res.status(204));
 
 app.get('/', (req, res) => {
   connection.query('SELECT * FROM roster', function (err, rows, fields) {
     if (err) throw err;
 
-    // Prepare the HTML table structure
-    let table = '<table>';
-    table += '<tr><th>Machine</th><th>Status</th><th>ETA</th></tr>';
-
-    // Iterate over the rows and populate the table
-    let availableCount = 0;
+    // Prepare the table rows HTML
+    let tableRows = '';
     rows.forEach((row) => {
       const rowClass = row.status === 'occupied' ? 'occupied-row' : '';
-      table += `<tr class="${rowClass}"><td>${row.machine}</td><td>${row.status}</td><td>${row.time_done}</td></tr>`;
-
-      if (row.status.endsWith('available')) {
-        availableCount++;
-      }
+      tableRows += `<tr class="${rowClass}"><td>${row.machine}</td><td>${row.status}</td><td>${row.time_done}</td></tr>`;
     });
 
-    // Check if there are available machines
+    // Calculate countText and countClass
+    let countText = '';
     let countClass = '';
+    let availableCount = rows.filter(row => row.status.endsWith('available')).length;
     if (availableCount > 0) {
-      const countText = `${availableCount} Washer${availableCount > 1 ? 's' : ''} Available`;
-      renderResponse(countText);
+      countText = `${availableCount} Washer${availableCount > 1 ? 's' : ''} Available`;
     } else {
-      connection.query('SELECT MIN(time_done) AS earliest_time FROM roster', function (err, rowse) {
-        if (err) throw err;
-        const earliestTime = rowse[0].earliest_time;
-        const countText = `Earliest Time Available: ${earliestTime}`;
-        renderResponse(countText);
-      });
+      const earliestTime = rows.reduce((minTime, row) => {
+        return row.time_done < minTime ? row.time_done : minTime;
+      }, rows[0].time_done);
+      countText = `Earliest Time Available: ${earliestTime}`;
       countClass = 'low-count';
     }
 
-    table += '</table>';
-
-    function renderResponse(countText) {
-      res.send(`
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>LarsonView</title> 
-            <link rel="icon" href="favicon.png" type="image/png">  
-            <link rel="icon" href="/favicon.ico" type="image/x-icon">
-            <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&display=swap" rel="stylesheet">
-            <style>
-              body {
-                text-align: center;
-                font-family: 'Fira Code', monospace;
-                background: #2C3A47;
-                color: #F8EFBA;
-              }
-
-              table {
-                margin: 0 auto;
-                border-collapse: collapse;
-                width: 80%;
-                background-color: rgba(88, 177, 159, 0.5);
-                padding: 12px;
-              }
-
-              th {
-                background-color: #222C36;
-                color: #F8EFBA;
-                padding: 8px;
-              }
-
-              td {
-                padding: 8px;
-              }
-
-              .count-container {
-                margin-top: 20px;
-                background-color: rgba(88, 177, 159, 0.8);
-                padding: 10px;
-                display: inline-block;
-              }
-
-              .count {
-                font-size: 20px;
-                color: #F8EFBA;
-              }
-
-              .low-count {
-                background-color: red;
-              }
-
-              .occupied-row {
-                background-color: red;
-              }
-            </style>
-            <script>
-              // Refresh the page every 10 seconds
-              setInterval(() => {
-                location.reload();
-              }, 10000);
-            </script>
-          </head>
-          <body>
-            <h1>LarsonView</h1>
-            <div class="count-container ${countClass}">
-              <div class="count">
-                ${countText}
-              </div>
-            </div>
-            <h2>Machine Roster</h2>
-            ${table}
-          </body>
-        </html>
-      `);
-    }
+    renderResponse(res, countText, tableRows, countClass);
   });
 });
+
+function renderResponse(res, countText, tableRows, countClass) {
+  const html = fs.readFileSync('./index.html', 'utf8');
+  const modifiedHtml = html
+    .replace('{{countText}}', countText)
+    .replace('{{tableRows}}', tableRows)
+    .replace('{{countClass}}', countClass);
+  
+  res.send(modifiedHtml);
+}
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
